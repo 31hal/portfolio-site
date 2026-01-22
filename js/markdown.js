9958e5049b271e67d1f2b6f89f31d3543f7b1224
31hal <104306579+31hal@users.noreply.github.com>
2026-01-22 09:52:34 +0900

    Update markdown.js

--- js/markdown.js
+++ js/markdown.js
@@ -1,4 +1,3 @@
-// 任意のページでMarkdownをロードしてHTML変換
 window.renderMarkdown = async function(mdFilePath, containerSelector) {
   try {
     const res = await fetch(mdFilePath);
@@ -6,12 +5,26 @@ window.renderMarkdown = async function(mdFilePath, containerSelector) {
     const mdText = await res.text();
 
     let html = mdText;
+
+    // 見出し
     html = html.replace(/^### (.+)$/gm, '<h3>$1</h3>');
     html = html.replace(/^## (.+)$/gm, '<h2>$1</h2>');
     html = html.replace(/^# (.+)$/gm, '<h1>$1</h1>');
-    html = html.replace(/^(?:\*|\-) (.+)$/gm, '<li>$1</li>');
+
+    // 箇条書き（連続するliを1つのulにまとめる）
+    html = html.replace(/^(?:\*|-)\s+(.+)$/gm, '<li>$1</li>');
     html = html.replace(/(<li>.*<\/li>)/gs, '<ul>$1</ul>');
-    html = html.replace(/\n/g, '<br>');
+
+    // 空行で段落を作る
+    html = html.replace(/\n{2,}/g, '</p><p>');
+    html = '<p>' + html + '</p>';
+
+    // 不要な <p> を調整
+    html = html
+      .replace('<p><ul>', '<ul>')
+      .replace('</ul></p>', '</ul>')
+      .replace(/<p>(<h[1-3]>)/g, '$1')
+      .replace(/(<\/h[1-3]>)<\/p>/g, '$1');
 
     const container = document.querySelector(containerSelector);
     if (container) container.innerHTML = html;
